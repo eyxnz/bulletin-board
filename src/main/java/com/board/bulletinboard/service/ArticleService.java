bc591b8e101e9dd4fd8b20b576b4f98aed8aa92f
eyxnz <todaytubuck@naver.com>
2023-03-05 17:22:06 +0900

    #7 - 게시글 서비스의 테스트와 기본 골격 설계
    들어갈 기능은 :
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID 로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제

---
+++ src/main/java/com/board/bulletinboard/service/ArticleService.java
@@ -0,0 +1,41 @@
+package com.board.bulletinboard.service;
+
+import com.board.bulletinboard.dto.ArticleDto;
+import com.board.bulletinboard.dto.ArticleUpdateDto;
+import com.board.bulletinboard.dto.type.SearchType;
+import com.board.bulletinboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    // SearchType 에 따른 게시글 조회
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+        return Page.empty();
+    }
+
+    // 특정 게시글 조회
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long articleId) {
+        return null;
+    }
+
+    // 게시글 저장
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    // 게시글 업데이트
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    // 게시글 삭제
+    public void deleteArticle(long articleId) {
+    }
+}
