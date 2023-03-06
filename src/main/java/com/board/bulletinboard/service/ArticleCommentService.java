a4afa230e667dd348791da0cf8fd02c5d4ad973f
eyxnz <todaytubuck@naver.com>
2023-03-06 14:45:55 +0900

    #11 - 댓글 비지니스 로직 구현 완료
    테스트가 모두 통과하는 것을 확인했다.

--- src/main/java/com/board/bulletinboard/service/ArticleCommentService.java
+++ src/main/java/com/board/bulletinboard/service/ArticleCommentService.java
@@ -4,11 +4,14 @@ import com.board.bulletinboard.dto.ArticleCommentDto;
 import com.board.bulletinboard.repository.ArticleCommentRepository;
 import com.board.bulletinboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -19,14 +22,23 @@ public class ArticleCommentService {
     // 특정 게시글의 댓글 조회
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream()
+                .map(ArticleCommentDto::from)
+                .toList();
     }
 
     // 댓글 저장
     public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto : {}", dto);
+        }
     }
 
     // 댓글 삭제
     public void deleteArticleComment(Long articleCommentId) {
+        articleCommentRepository.deleteById(articleCommentId);
     }
 }
