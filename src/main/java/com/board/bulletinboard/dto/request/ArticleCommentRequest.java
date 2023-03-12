4b7f4eee05ba45185bd1b72d92d847195992bbd8
eyxnz <todaytubuck@naver.com>
2023-03-12 10:33:47 +0900

    #23 - 댓글 저장, 삭제 기능 구현

---
+++ src/main/java/com/board/bulletinboard/dto/request/ArticleCommentRequest.java
@@ -0,0 +1,21 @@
+package com.board.bulletinboard.dto.request;
+
+import com.board.bulletinboard.dto.ArticleCommentDto;
+import com.board.bulletinboard.dto.UserAccountDto;
+
+public record ArticleCommentRequest(
+        Long articleId,
+        String content
+) {
+    public static ArticleCommentRequest of(Long articleId, String content) {
+        return new ArticleCommentRequest(articleId, content);
+    }
+
+    public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
+        return ArticleCommentDto.of(
+                articleId,
+                userAccountDto,
+                content
+        );
+    }
+}
